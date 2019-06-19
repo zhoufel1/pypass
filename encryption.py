ff691df53003a1d1e9cbc11fe1f176e0023a3fd1
Felix <felix.rn.zhou@gmail.com>
2019-06-18 23:44:20 -0400

    Update type contracts

--- encryption.py
+++ encryption.py
@@ -8,7 +8,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
-def key_generator(password: str):
+def key_generator(password: str) -> bytes:
     """Return bytes representing a generated key"""
     encoded_pass = password.encode()
     salt = b'\xbf\x07\xfc\xb2Y\x80\xbas\xb4\x02\x0f\x84\x9da\xb3\xfb'
@@ -22,20 +22,20 @@ def key_generator(password: str):
     return base64.urlsafe_b64encode(kdf.derive(encoded_pass))
 
 
-def encrypt_password(password: str, key: bytes):
+def encrypt_password(password: str, key: bytes) -> str:
     """Return a string representing a password encrypted using Fernet given the key to be
     stored in the database"""
     fernet_obj = Fernet(key)
     return fernet_obj.encrypt(password.encode()).decode('utf-8')
 
 
-def decrypt_password(encrypted_pass: str, key: bytes):
+def decrypt_password(encrypted_pass: str, key: bytes) -> str:
     """Return a string representing a password retrieved from the database decrypted using
     Fernet given the key"""
     fernet_obj = Fernet(key)
     return fernet_obj.decrypt(encrypted_pass.encode()).decode('utf-8')
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> bytes:
     """Returns bytes representing a hashed password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
