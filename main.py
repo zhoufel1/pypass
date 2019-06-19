f71a8c0209014c441092ee945b09ef844a239c20
Felix <felix.rn.zhou@gmail.com>
2019-06-18 23:47:20 -0400

    Update type contracts

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class PasswordError(Exception):
         super().__init__(message)
 
 
-def run():
+def run() -> None:
     """Initialize the program"""
 
     # Create database handler object
@@ -85,7 +85,7 @@ def run():
                            "[6] Exit\n")
 
 
-def create_database(database_handler):
+def create_database(database_handler) -> bool:
     """Return True if the database already exist. If not, create the database
     and return False"""
     if not os.path.exists('./account_database.db'):
@@ -96,7 +96,7 @@ def create_database(database_handler):
     return True
 
 
-def handle_password(trigger: bool, database_handler):
+def handle_password(trigger: bool, database_handler) -> str:
     """Return the user-inputted password as a string. If database is
     already created, check the inputted password with database password.
     Otherwise, prompt user to create a password for the database"""
@@ -104,6 +104,7 @@ def handle_password(trigger: bool, database_handler):
         p_input = getpass("Enter your password: ")
         if not checkpw(p_input.encode(), database_handler.retrieve_password()):
             raise Exception("Incorrect password")
+        return p_input
     else:
         while True:
             p_input = getpass("Enter a password for database: ")
@@ -112,10 +113,10 @@ def handle_password(trigger: bool, database_handler):
                 database_handler.set_password(hash_password(p_input))
                 break
             print("*Passwords do not match*")
-    return p_input
+        return p_input
 
 
-def show_search_query(database_handler, key: bytes):
+def show_search_query(database_handler, key: bytes) -> None:
     """Prompt the user to enter a search query and print all
     account information associated with that site. If multiple
     queries are found, have the user select."""
@@ -147,7 +148,7 @@ def show_search_query(database_handler, key: bytes):
             os.system('clear')
 
 
-def show_all_data(database_handler, key: bytes):
+def show_all_data(database_handler, key: bytes) -> None:
     """Print all account information in the database"""
     queries = database_handler.query_all_entries()
     print("\n============Account Info============")
@@ -158,7 +159,7 @@ def show_all_data(database_handler, key: bytes):
     print("====================================")
 
 
-def handle_data_input(database_handler, key: bytes):
+def handle_data_input(database_handler, key: bytes) -> None:
     """Prompts the user to enter account information and store it into the
     Account table in database as a row"""
     site = input("\nEnter site: ").lower().strip(" ")
@@ -177,7 +178,7 @@ def handle_data_input(database_handler, key: bytes):
             print("Invalid entry")
 
 
-def handle_data_update(database_handler, key: bytes):
+def handle_data_update(database_handler, key: bytes) -> None:
     """Prompts the user to enter account information to update that row
     with a new generated password"""
     if database_handler.is_empty():
@@ -208,7 +209,7 @@ def handle_data_update(database_handler, key: bytes):
             print("Password copied!")
 
 
-def handle_data_delete(database_handler):
+def handle_data_delete(database_handler) -> None:
     """Handles row deletion"""
     if database_handler.is_empty():
         print("Database is empty...")
@@ -235,7 +236,7 @@ def handle_data_delete(database_handler):
             time.sleep(1)
             os.system('clear')
 
-def handle_table_delete(database_handler):
+def handle_table_delete(database_handler) -> None:
     """Handles table deletion"""
     password = getpass("You are about to wipe account info. " +
                        "Enter password to confirm: ")
@@ -248,7 +249,7 @@ def handle_table_delete(database_handler):
 
 # ======================Search Menu Logic========================
 
-def invoke_menu(input_list: list):
+def invoke_menu(input_list: list) -> None:
     """
     Display the meny of options and prompt the user
     to select a valid option. Retrieve the password
@@ -260,7 +261,6 @@ def invoke_menu(input_list: list):
         print("No items found")
         time.sleep(0.5)
         os.system('clear')
-        return None
     show_menu(options)
     print('\n')
     user_input = input("Which account? ").strip()
@@ -269,7 +269,7 @@ def invoke_menu(input_list: list):
     return options[int(user_input)]
 
 
-def show_menu(menu_options: dict):
+def show_menu(menu_options: dict) -> None:
     """
     Show the options in the menu_options.
     """
@@ -278,7 +278,7 @@ def show_menu(menu_options: dict):
               '\n    User: ' + menu_options[item].username)
 
 
-def build_menu_options(input_list: list):
+def build_menu_options(input_list: list) -> list:
     """Return a dictionary wherein the keys
     correspond to integer input values starting from 0
     the values are the account information."""
