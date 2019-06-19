fba2af814ea7a75a3ba37a83d71eb96e0da633e9
Felix <felix.rn.zhou@gmail.com>
2019-06-18 23:40:33 -0400

    Update type contracts for functions/methods

--- database_handler.py
+++ database_handler.py
@@ -10,8 +10,8 @@ class DatabaseHandler():
     """A class used for handling database operations"""
     # Base class for tables
     Base = declarative_base()
-    # Account class for tables
 
+    # Account class for tables
     class Account(Base):
         """An account in the database."""
         __tablename__ = 'account'
@@ -31,25 +31,26 @@ class DatabaseHandler():
         id = Column(Integer, primary_key=True)
         password = Column(String)
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize the database handler"""
         self.engine = create_engine('sqlite:///account_database.db')
         self.Base.metadata.bind = self.engine
         self.DBSession = sessionmaker(bind=self.engine)
         self.session = self.DBSession()
 
-    def create_database(self):
+    def create_database(self) -> None:
         """Creates an sqlite database"""
         self.Base.metadata.create_all(self.engine)
-    # database manipulation
 
-    def insert_data(self, site: str, username: str, password: str):
+    def insert_data(self, site: str,
+                          username: str, password: str) -> None:
         """Inserts the site, username, and password into the database"""
         self.session.add(self.Account(site=site,
                                       username=username, password=password))
         self.session.commit()
 
-    def query_database(self, site: str = None, username: str = None):
+    def query_database(self,
+                       site: str = None, username: str = None) -> dict:
         """
         Return all queries in the database.
         """
@@ -58,7 +59,7 @@ class DatabaseHandler():
         results = [x for x in query_all]
         return results
 
-    def query_site_and_user(self, site: str, username: str):
+    def query_site_and_user(self, site: str, username: str) -> dict:
         """Return an account with site and username."""
         results = {}
         query = self.session.query(self.Account).\
@@ -72,7 +73,7 @@ class DatabaseHandler():
                 results[instance.site].append(instance)
         return results
 
-    def query_all_entries(self):
+    def query_all_entries(self) -> dict:
         """Return a dictionary of queries, where each
         key denotes a site and the values are the
         websites associated with the key."""
@@ -85,12 +86,13 @@ class DatabaseHandler():
             results[item] = [x for x in query]
         return results
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """Return True if the database is empty, False otherwise."""
         results = self.query_database()
         return len(results) == 0
 
-    def update_item(self, site: str, username: str, new_password: str):
+    def update_item(self,
+                    site: str, username: str, new_password: str) -> None:
         """Update the row with site and username with the new_password"""
         query = self.session.query(self.Account).\
             filter(self.Account.site == site).\
@@ -98,24 +100,24 @@ class DatabaseHandler():
         query[0].password = new_password
         self.session.commit()
 
-    def drop_tables(self):
+    def drop_tables(self) -> None:
         """Drop all tables in the database"""
         self.session.query(self.Account).delete()
         self.session.commit()
 
-    def delete_row(self, site: str, username: str):
+    def delete_row(self, site: str, username: str) -> None:
         """Delete the row in the database with site and username"""
         self.session.query(self.Account).filter(self.Account.site == site).\
             filter(self.Account.username == username).delete()
         self.session.commit()
 
-    def set_password(self, password: str):
+    def set_password(self, password: str) -> None:
         """Add password to the password table"""
         self.session.add(self.Password(password=password))
         self.session.commit()
 
-    def retrieve_password(self):
-        """Return a string representing the password retrieved from the
+    def retrieve_password(self) -> bytes:
+        """Return bytes representing the password retrieved from the
         password table"""
         query = self.session.query(self.Password).all()
         return query[0].password
