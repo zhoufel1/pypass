97db3bc5ab245b79327299483ea0f8e9718d6530
Felix <felix.rn.zhou@gmail.com>
2019-06-19 21:13:04 -0400

    Update imports to import entire modules

--- encryption.py
+++ encryption.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import base64
 import bcrypt
-from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography import fernet
+from cryptography.hazmat import backends
+from cryptography.hazmat import primitives
+from cryptography.hazmat.primitives.kdf import pbkdf2
 
 
 def key_generator(password: str) -> bytes:
@@ -14,12 +14,12 @@ def key_generator(password: str) -> bytes:
 
     encoded_pass = password.encode()
     salt = b'\xbf\x07\xfc\xb2Y\x80\xbas\xb4\x02\x0f\x84\x9da\xb3\xfb'
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
+    kdf = pbkdf2.PBKDF2HMAC(
+        algorithm=primitives.hashes.SHA256(),
         length=32,
         salt=salt,
         iterations=100000,
-        backend=default_backend()
+        backend=backends.default_backend()
     )
     return base64.urlsafe_b64encode(kdf.derive(encoded_pass))
 
@@ -29,7 +29,7 @@ def encrypt_password(password: str, key: bytes) -> str:
     Return a string representing the encrypted <password> given the <key>.
     """
 
-    fernet_obj = Fernet(key)
+    fernet_obj = fernet.Fernet(key)
     return fernet_obj.encrypt(password.encode()).decode('utf-8')
 
 
@@ -39,7 +39,7 @@ def decrypt_password(encrypted_pass: str, key: bytes) -> str:
     <key>.
     """
 
-    fernet_obj = Fernet(key)
+    fernet_obj = fernet.Fernet(key)
     return fernet_obj.decrypt(encrypted_pass.encode()).decode('utf-8')
 
 
