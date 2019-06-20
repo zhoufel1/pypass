3a70e33a22a68b9aba2b4bd675cf1bba64a68b81
Felix <felix.rn.zhou@gmail.com>
2019-06-19 21:25:34 -0400

    Clean up encrypt_password and decrypt_password

--- encryption.py
+++ encryption.py
@@ -12,14 +12,15 @@ def key_generator(password: str) -> bytes:
     from the <password>.
     """
 
-    kdf = pbkdf2.PBKDF2HMAC(
+    password_encoded = password.encode()
+    key_deriver = pbkdf2.PBKDF2HMAC(
         algorithm=primitives.hashes.SHA256(),
         length=32,
         salt=b'\xbf\x07\xfc\xb2Y\x80\xbas\xb4\x02\x0f\x84\x9da\xb3\xfb',
         iterations=100000,
         backend=backends.default_backend()
         )
-    return base64.urlsafe_b64encode(kdf.derive(password.encode()))
+    return base64.urlsafe_b64encode(key_deriver.derive(password_encoded))
 
 
 def encrypt_password(password: str, key: bytes) -> str:
@@ -27,8 +28,8 @@ def encrypt_password(password: str, key: bytes) -> str:
     Return a string representing the encrypted <password> given the <key>.
     """
 
-    fernet_obj = fernet.Fernet(key)
-    return fernet_obj.encrypt(password.encode()).decode('utf-8')
+    fernet_object = fernet.Fernet(key)
+    return fernet_object.encrypt(password.encode()).decode('utf-8')
 
 
 def decrypt_password(encrypted_pass: str, key: bytes) -> str:
@@ -37,8 +38,8 @@ def decrypt_password(encrypted_pass: str, key: bytes) -> str:
     <key>.
     """
 
-    fernet_obj = fernet.Fernet(key)
-    return fernet_obj.decrypt(encrypted_pass.encode()).decode('utf-8')
+    fernet_object = fernet.Fernet(key)
+    return fernet_object.decrypt(encrypted_pass.encode()).decode('utf-8')
 
 
 def hash_password(password: str) -> bytes:
