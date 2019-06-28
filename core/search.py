cd9510ea4ddbe7f20c8a2555c81648e3569d7956
Felix <felix.rn.zhou@gmail.com>
2019-06-27 23:39:55 -0400

    Remove unnecessary docstring

--- core/search.py
+++ core/search.py
@@ -2,11 +2,6 @@ from typing import List
 
 
 def edit_distance(source: str, target: str) -> int:
-    """
-    Return an integer representing the edit (or Levenshtein) distance
-    coefficient between the <source> string and the <target> string.
-    """
-
     len_source = len(source)
     len_target = len(target)
 
@@ -18,21 +13,11 @@ def edit_distance(source: str, target: str) -> int:
 
 
 def build_empty_matrix(row: int, col: int) -> List[list]:
-    """
-    Return a nested list representing an empty matrix
-    that is <row> by <col>.
-    """
-
     return [[0 for x in range(col + 1)]
             for y in range(row + 1)]
 
 
 def build_first_row_col(row: int, col: int, matrix: List[list]) -> None:
-    """
-    Update the first row and columns of the <matrix>
-    to the indexes of <row> and <col>.
-    """
-
     for i in range(1, row + 1):
         matrix[i][0] = i
     for j in range(1, col + 1):
@@ -44,10 +29,6 @@ def calculate_diagonal(row: int,
                        matrix: List[list],
                        source: str,
                        target: str) -> None:
-    """Calculate the diagonal of the matrix in order to find
-    the Levenshtein distance.
-    """
-
     for i in range(1, row + 1):
         for j in range(1, col + 1):
             if source[i - 1] == target[j - 1]:
@@ -60,11 +41,6 @@ def calculate_diagonal(row: int,
 
 
 def is_found(source: str, target: str) -> bool:
-    """Use the edit distance to fuzzy search
-    for <source> in <target>. Return True if <source> is
-    found, False otherwise.
-    """
-
     for i in range(len(target)):
         if source[0] == target[i] and len(target) - i - 1 >= len(source) - 1:
             snippet = target[i:i + len(source)]
