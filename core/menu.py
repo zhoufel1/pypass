cf0a79547f170db020e588c4b1218a80fcc36b05
Felix <felix.rn.zhou@gmail.com>
2019-06-27 23:40:33 -0400

    Add menu building functinos to menu module

--- core/menu.py
+++ core/menu.py
@@ -2,61 +2,47 @@
 
 from __future__ import annotations
 from typing import Callable, Optional
+import data_manipulation as dm
 
 
 class MenuObject:
-    """A base class to represent objects inside a menu."""
 
     def __init__(self, title: str) -> None:
-        """Initialize the MenuObject object given the <title>."""
         self.title = title
         self.selected = False
 
     def __repr__(self) -> str:
-        """Return a string representation of the MenuObject."""
         if self.selected:
             return f'> {self.title}'
         return f'  {self.title}'
 
 
 class Option(MenuObject):
-    """An option in a menu that a user can select."""
 
     def __init__(self, title: str, func: Callable) -> None:
-        """Initialize the option object."""
         super().__init__(title)
         self.func = func
 
 
 class Menu(MenuObject):
-    """A menu that a user can interact with. A menu can exist
-    as a submenu of another parent menu."""
 
     def __init__(self, title: str, parent: Optional[Menu] = None):
-        """Initialize an empty menu object. If <parent> is None,
-        then the menu is base menu. Otherwise, it is a submenu
-        to <parent>."""
         super().__init__(title)
         self.parent = parent
         self.options = []
         self.pointer = None
 
     def add_option(self, option: MenuObject) -> None:
-        """Add the <option> to the menu. If the menu is
-        currently empty, select <option> and point to it."""
         if not self.options:
             self.pointer = option
             self.pointer.selected = True
         self.options.append(option)
 
     def print_options(self) -> None:
-        """Print the items in the menu."""
         for item in self.options:
             print(item)
 
     def point_next(self) -> None:
-        """Point to the next option in the menu if it exists.
-        Otherewise, do nothing."""
         if self.options.index(self.pointer) + 1 == len(self.options):
             return None
         else:
@@ -65,11 +51,36 @@ class Menu(MenuObject):
             self.pointer.selected = True
 
     def point_prev(self) -> None:
-        """Point to the previous option in the menu if it exists.
-        Otherwise, do nothing."""
         if self.options.index(self.pointer) - 1 < 0:
             return None
         else:
             self.pointer.selected = False
             self.pointer = self.options[self.options.index(self.pointer) - 1]
             self.pointer.selected = True
+
+
+def build_main_menu() -> Menu:
+    base_menu = Menu(None)
+    base_menu.add_option(build_search_menu(base_menu))
+    base_menu.add_option(build_input_menu(base_menu))
+    base_menu.add_option(Option("Update existing entry",
+                         dm.update_data))
+    base_menu.add_option(Option('Delete existing entry',
+                         dm.delete_data))
+    base_menu.add_option(Option('Reset database', dm.delete_all))
+    return base_menu
+
+
+def build_search_menu(base_menu: Menu) -> Menu:
+    search_menu = Menu('Show entries', base_menu)
+    search_menu.add_option(Option('Search', dm.show_search))
+    search_menu.add_option(Option('Show all', dm.show_all))
+    return search_menu
+
+
+def build_input_menu(base_menu: Menu) -> Menu:
+    input_menu = Menu('Add new entry', base_menu)
+    input_menu.add_option(Option('Generate password', dm.input_data))
+    input_menu.add_option(Option('Enter existing password',
+                          dm.input_existing_data))
+    return input_menu
