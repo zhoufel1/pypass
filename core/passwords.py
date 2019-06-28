87ee7a2fa647bd096086f9a1de508b58fa85e560
Felix <felix.rn.zhou@gmail.com>
2019-06-28 18:31:55 -0400

    Use list comprehension instead of while loop

--- core/passwords.py
+++ core/passwords.py
@@ -6,15 +6,14 @@ LOWER = string.ascii_lowercase
 
 
 def generate_password(length: int) -> str:
-    password = ''
-    index = 0
-    while index < length:
-        character_type = random.randint(0, 2)
-        if character_type == 0:
-            password += LOWER[random.SystemRandom().randint(0, 25)]
-        elif character_type == 1:
-            password += UPPER[random.SystemRandom().randint(0, 25)]
-        else:
-            password += str(random.SystemRandom().randint(0, 9))
-        index += 1
-    return password
+    return ''.join([determine_type(random.randint(0, 2))
+                   for i in range(length)])
+
+
+def determine_type(value: int) -> str:
+    if value == 0:
+        return LOWER[random.SystemRandom().randint(0, 25)]
+    elif value == 1:
+        return UPPER[random.SystemRandom().randint(0, 25)]
+    else:
+        return str(random.SystemRandom().randint(0, 9))
