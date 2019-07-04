26b8f16326d9f11ab32003852babd531b7ffc7a8
Felix <felix.rn.zhou@gmail.com>
2019-07-03 21:51:58 -0400

    determine_type to determine_character_type

--- core/passwords.py
+++ core/passwords.py
@@ -3,11 +3,11 @@ from constants import UPPER, LOWER
 
 
 def generate_password(length: int) -> str:
-    return ''.join([determine_type(random.randint(0, 2))
+    return ''.join([determine_character_type(random.randint(0, 2))
                    for i in range(length)])
 
 
-def determine_type(value: int) -> str:
+def determine_character_type(value: int) -> str:
     if value == 0:
         return LOWER[random.SystemRandom().randint(0, 25)]
     elif value == 1:
